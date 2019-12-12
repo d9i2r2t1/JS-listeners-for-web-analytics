14ae251fd20e27328a4d7faafad855f11df0b36e
dirtrider <dirt-rider@yandex.ru>
2019-12-12 13:37:36 +0300

    Add ajax.open listener

--- listener_AJAX.js
+++ listener_AJAX.js
@@ -1,7 +1,21 @@
 (function() {
     if (window.ajax_listener !== 1) {
+        // Слушаем открытие соединения с сервером
+        var open = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function() { 
+            // Аргументы, с которыми было открыто соединение
+            console.log('AJAX open:', arguments);
+            console.log('Request type =', arguments[0]);
+            console.log('Request URL =', arguments[1]);
+            console.log('Is request async =', arguments[2]);
+            return open.apply(this, arguments);
+        };
+
+        // Слушаем отправку запроса
         var send = XMLHttpRequest.prototype.send;
         XMLHttpRequest.prototype.send = function() {
+            // Аргументы, с которыми был отправлен запрос
+            console.log('AJAX send:', arguments);
             this.addEventListener('load', function() {
                 // Когда запрос успешно обработается сервером - выполняем нужные действия
                 if(this.status === 200) {
